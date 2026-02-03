7b97aa244d960370bef76a0fc35228a90616b66d
Manan Ranka <mananranka217@gmail.com>
2026-02-03 23:31:09 +0530

    vercel deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      // Alias @ to the src directory
-      base: "/birthday_wish/",
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
