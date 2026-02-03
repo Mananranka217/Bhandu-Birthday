337577d3aad57dc3e05761e17e1d7262bbb0f32a
Manan Ranka <mananranka217@gmail.com>
2026-02-03 22:22:39 +0530

    initial push

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Alias @ to the src directory
+      base: "/birthday_wish/",
       '@': path.resolve(__dirname, './src'),
     },
   },
